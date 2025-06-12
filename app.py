6af013681d2352192bc4b4b780be9805428fb4b9
sungpil <ippo252525@gmail.com>
2025-06-12 23:42:16 +0900

    Test : streamlit cloud

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
+import ssl
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import psycopg2
 
-# CSV 로드
-df = pd.read_csv("data/sample.csv")
+# PSQL Connection
+conn = psycopg2.connect(
+    host=st.secrets["DB_HOST"],
+    port=st.secrets["DB_PORT"],
+    dbname=st.secrets["DB_NAME"],
+    user=st.secrets["DB_USER"],
+    password=st.secrets["DB_PASSWORD"],
+    sslmode='require'
+)
+query = "SELECT * FROM mv_fame_distribution_1k;"
+df = pd.read_sql(query, conn)
 
-st.title("Plotly 그래프 예제")
-
-# 데이터 표
-st.subheader("Raw Data")
-st.dataframe(df)
+st.title("PSQL Test Dashboard")
 
 # 선 그래프
 st.subheader("Line Chart (Plotly)")
-fig_line = px.line(df, y="value", title="Line Chart")
-st.plotly_chart(fig_line)
-
-# 바 그래프
-st.subheader("Bar Chart (Plotly)")
-fig_bar = px.bar(df, y="value", title="Bar Chart")
-st.plotly_chart(fig_bar)
+fig = px.bar(
+    df,
+    x="fame_bin",
+    y="count",
+    title="명성 1000단위 분포",
+    labels={"fame_bin": "명성 구간", "count": "캐릭터 수"},
+)
+st.plotly_chart(fig)
